58e900bbfb7a07706b9c28cd7d3a570a99c50df5
gkobilansky <genek149@gmail.com>
2015-10-29 12:08:06 -0400

    formatting and dates

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-var jqxhr = $.get("http://www.freecodecamp.com/news/hot");
+$(document).ready(function () {
 
+    var jqxhr = $.get("http://www.freecodecamp.com/news/hot");
 
+    jqxhr.success(function () {
 
-jqxhr.success(function () {
+        var posts = jqxhr.responseJSON;
 
-    var posts = jqxhr.responseJSON;
-    console.log(posts[0].headline);
+        posts.forEach(function (element) {
 
-    posts.forEach(function (element) {
+            var date = new Date(element.timePosted).toGMTString().slice(0, -13);
+            var storyPic = element.image;
+            if (!storyPic) {
+                storyPic = "http://placehold.it/120x120?text=FreeCodeCamp.com";
+            };
 
-        $(".media-list").append(
-            '<li class="media"><a class="pull-left" href="' + element.link + '"><img class="media-object" data-src="holder.js/64x64" src="' + element.image + '" alt="' + element.heading + '" style="width: 64px; height: 64px;"></a><div class="media-body"><h4 class="media-heading">' + element.headline + '</h4></div>' + element.metaDescription + '</li>'
+            var html = '<li class="media"><a class="pull-left" href="' + element.link + '">';
+            html += '<img class="media-object" src="' + storyPic + '" alt="' + element.heading + '" style="width: 120px; height: 120px;"></a>';
+            html += '<div class="media-body"><a href="' + element.link + '"><h4 class="media-heading">' + element.headline + '</h4></a></div>';
+            html += '<span class="glyphicon glyphicon-fire"></span> ' + element.upVotes.length;
+            html += '<p>' + date + '</p>';
+            html += '<p>' + element.metaDescription + '</p>';
+            html += '<a class="btn btn-info btn-sm pull-right" href="http://freecodecamp.com/' + element.author.username + '">@' + element.author.username + '</a></li>';
 
-        )
+            $(".media-list").append(html);
+        });
     });
+
+
+    // menu
+
+    $('.nav li').click(function () {
+        $('.nav li.active').removeClass('active');
+        var $this = $(this);
+        if (!$this.hasClass('active')) {
+            $this.addClass('active');
+        }
+    });
+
 });
